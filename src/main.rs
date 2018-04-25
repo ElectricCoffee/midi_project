f26813d4c770ce45174e58e2ecc80ab924c817fe
Niko Lepka <slench102+git@gmail.com>
2018-04-25 14:30:32 +0200

    Add documentation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use std::fmt;
 
 // constants
+/// The duration of a whole note in MIDI ticks.
 const MIDI_TEMPO: f32 = 3840.0;
 
 // traits
@@ -20,8 +21,12 @@ impl<T> Cloneable for T where T: 'static + MusicElement + Clone {
     }
 }
 
+/// All `MusicElement`s need the ability to output a duration and set the channel retroactively
 trait MusicElement : fmt::Debug + Cloneable {
+    /// Returns the musical duration of a `MusicElement`
+    /// Durations are implemented as `f32` due to the fractional nature of music.
     fn duration(&self) -> f32;
+    /// Sets the channel to something else.
     fn set_channel(&mut self, channel: Channel);
 }
 
@@ -33,6 +38,8 @@ impl Clone for Box<MusicElement> {
 
 // enums
 #[derive(Debug, Clone)]
+/// Specifies the different note names available
+/// TODO: implement From<String> on this, to allow things like "Do".into() or "C#".into()
 enum NoteClass {
     C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B
 }
@@ -57,11 +64,14 @@ impl fmt::Display for NoteClass {
 }
 
 #[derive(Debug, Clone)]
+/// Channel lists the available musical channels to play on
+/// This list is the only instruments used in the original project, and thus do not contain the full list of instruments
 enum Channel {
     Piano, Organ, Guitar, Violin, Flute, Trumpet, Helicopter, Telephone
 }
 
 impl Channel {
+    /// Converts the channel to a number
     fn to_u32(self) -> u32 {
         self as u32 + 1
     }
@@ -75,6 +85,7 @@ struct MidiNote {
 }
 
 #[derive(Debug, Clone)]
+/// The basic `Note` struct, which contains all the relevant information
 struct Note {
     name: NoteClass,
     octave: u32,
@@ -83,20 +94,24 @@ struct Note {
 }
 
 impl Note {
+    /// Creates a new note with the instrument set to `Piano` and duration to 1/4 by default.
     fn new(name: NoteClass, octave: u32) -> Note {
         Note { name, octave, instrument: Channel::Piano, duration: 0.25 }
     }
-    
+
+    /// Builder method that adds a channel to a `Note`
     fn channel(mut self, instrument: Channel) -> Note {
         self.instrument = instrument;
         self
     }
-    
+
+    /// Builder method that adds a duration to a `Note`
     fn duration(mut self, duration: f32) -> Note {
         self.duration = duration;
         self
     }
-    
+
+    /// Converts a `Note` to a `MidiNote`
     fn to_midi(&self) -> MidiNote {
         let offset = self.name.clone() as u32;
         let pitch = (12 * self.octave) + offset;
@@ -115,11 +130,13 @@ impl MusicElement for Note {
 }
 
 #[derive(Debug, Clone)]
+/// A simple pause, only contains a duration
 struct Pause {
     duration: f32,
 }
 
 impl Pause {
+    /// Initialises a pause with a duration
     fn new(duration: f32) -> Pause {
         Pause { duration }
     }
@@ -134,6 +151,7 @@ impl MusicElement for Pause {
 }
 
 #[derive(Debug, Default, Clone)]
+/// Defines a sequential composition --- a series of notes next to each other
 struct Sequential {
     elements: Vec<Box<MusicElement>>,
 }
@@ -150,6 +168,7 @@ impl MusicElement for Sequential {
     }
 }
 
+/// Helper macro that boxes all its inputs before passing them into the vector
 macro_rules! sequence {
     ($($e:expr),*) => ({
         let mut seq = Sequential::default();
@@ -161,6 +180,7 @@ macro_rules! sequence {
 }  
 
 #[derive(Debug, Default, Clone)]
+/// Defines a parallel composition --- notes that are on top of each other (chords, etc)
 struct Parallel {
     elements: Vec<Box<MusicElement>>,
 }
@@ -178,6 +198,7 @@ impl MusicElement for Parallel {
     }
 }
 
+/// Helper macro for parallel compositions, packs the inputs in a box before moving them into the vector
 macro_rules! parallel {
     ($($e:expr),*) => ({
         let mut seq = Parallel::default();
@@ -189,6 +210,7 @@ macro_rules! parallel {
 }  
 
 fn main() {
+    // defines helper functions for creating notes
     let c4 = |dur| Note::new(NoteClass::C, 4).duration(dur);
     let d4 = |dur| Note::new(NoteClass::D, 4).duration(dur);
     let e4 = |dur| Note::new(NoteClass::E, 4).duration(dur);
@@ -196,6 +218,7 @@ fn main() {
     let g4 = |dur| Note::new(NoteClass::G, 4).duration(dur);
     let c5 = |dur| Note::new(NoteClass::C, 5).duration(dur);
 
+    // defines the triplets used in the song
     let c5_triplet = sequence![c5(1.0/12.0), c5(1.0/12.0), c5(1.0/12.0)];
     let g4_triplet = sequence![g4(1.0/12.0), g4(1.0/12.0), g4(1.0/12.0)];
     let e4_triplet = sequence![e4(1.0/12.0), e4(1.0/12.0), e4(1.0/12.0)];
@@ -207,6 +230,7 @@ fn main() {
                           c5_triplet, g4_triplet, e4_triplet, c4_triplet,
                           g4(3.0/16.0), f4(1.0/16.0), e4(3.0/16.0), d4(1.0/16.0), c4(0.5)];
 
+    // change the channel to organ and violin for the offset parts of the canon
     let mut music_organ  = music.clone();
     let mut music_violin = music.clone();
     music_organ.set_channel(Channel::Organ);
